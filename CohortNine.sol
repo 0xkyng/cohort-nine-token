e9f67b895bebe7ad9efcbe8b844f3efcf107cc55
Isaac <teryima127@gmail.com>
2023-08-22 23:02:42 +0100

    implemented  functions

--- CohortNine.sol
+++ CohortNine.sol
@@ -14,6 +14,11 @@ contract CohortNine {
 
     mapping(address => mapping(address => uint)) _allowance;
 
+    event Transfer(address from, address to, uint value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Minted(address to, uint value);
+
+
     modifier onlyOwner() {
         require(msg.sender == owner, "must be the owner");
         _;
@@ -40,6 +45,68 @@ contract CohortNine {
     function totalSupply() public view returns(uint) {
         return _totalsupply;
     }
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+    return _balance[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+    require(_to != address(0), "transfer to address ZERO not allowed");
+    require(_value > 0, "increase value");
+    require(balanceOf(msg.sender) >= _value, "insufficient funds");
+    _balance[msg.sender] -= _value;
+    _balance[_to] += _value;
+    success = true;
+    emit Transfer(msg.sender, _to, _value);
+
+}
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+    require(_to != address(0), "transfer to address ZERO not allowed");
+    require(_value > 0, "increase value");
+    require(balanceOf(_from) >= _value, "insufficient funds");
+    require(_value <= allowance(_from, _to) , "insufficient allowance");
+    _allowance[_from][_to] -= _value;
+    _balance[_from] -= _value;
+    _balance[_to] += _value;
+    success = true;
+    emit Transfer(_from, _to, _value);
+}
+
+function approve(address _spender, uint256 _value) public returns (bool success){
+    _allowance[msg.sender][_spender] = _value;
+    success = true;
+    emit Approval(msg.sender, _spender, _value);
+
+}
+
+function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+    return _allowance[_owner][_spender];
+}
+  function mint(address _to, uint value) external payable  {
+    require(_to != address(0), "transfer to address ZERO not allowed");
+    require(msg.value == 5 ether * value, "less than the required amount");
+
+    _totalSupply += value;
+    _balance[_to] += value; 
+
+    emit Minted(_to, value);
+  }
+
+  function burn( address _to,uint value) external{
+    require(balanceOf(msg.sender) >= value, "insufficient funds");
+    _balance[msg.sender] -= value; 
+    _totalSupply -= value * 90/100;
+
+    uint remainingValue = value * 10/100;
+    _balance[_to] += remainingValue;
+    
+  }
+
+  function withdrawAll() external onlyOwner {
+    payable(owner).transfer(address(this).balance);
+}
+
+
 
     
 
