5507b595c4eefedc5ea5451e6bbe521475913fc1
Isaac <teryima127@gmail.com>
2023-08-22 22:57:31 +0100

    implemented  functions

--- CohortNine.sol
+++ CohortNine.sol
@@ -25,5 +25,23 @@ contract CohortNine {
         owner = msg.sender;
     }
 
+    function name() public view returns(string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns(string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure returns(uint) {
+        return DECIMAL;
+    }
+
+    function totalSupply() public view returns(uint) {
+        return _totalsupply;
+    }
+
+    
+
 
 }
