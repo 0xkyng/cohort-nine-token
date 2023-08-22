d638c52d736e80904b5029cf699ba0c0ebc4f32a
Isaac <teryima127@gmail.com>
2023-08-22 22:40:39 +0100

    state variables

---
+++ CohortNine.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+
+contract CohortNine {
+
+    string _name;
+    string _symbol;
+    uint constant DECIMAL = 18;
+    uint _totalsupply;
+    address public owner;
+    
+
+}
