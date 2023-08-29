0a307cccef91d8af36fff628d97ca83afc0867ad
Isaac <teryima127@gmail.com>
2023-08-29 22:40:16 +0100

    constructor

--- Token.sol
+++ Token.sol
@@ -1,10 +1,8 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import {ERC20}  from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 100e18);
+///SPDX-License-Identifier: MIT
+import {ERC20} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.0; 
+contract StandardToken is ERC20{
+    constructor() ERC20("na", "sy"){
+        _mint(address(this),1_000e8);
     }
 }
