e4babf529373fe2340351d17099b36c92b7d5394
Isaac <teryima127@gmail.com>
2023-08-29 22:41:19 +0100

    decimals

--- Token.sol
+++ Token.sol
@@ -5,4 +5,13 @@ contract StandardToken is ERC20{
     constructor() ERC20("na", "sy"){
         _mint(address(this),1_000e8);
     }
+
+    // 20000e18
+    uint ExpectedAmount = 20_000 ether; 
+    uint precision = 1e20;
+
+     function decimals() public view override returns (uint8) {
+        return 8;
+    }
+
 }
